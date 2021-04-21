eb141152e9b0d7b04c5c6f2a133b70b5b21b610a
Sherfin Shamsudeen <sherfin94@gmail.com>
2021-04-21 19:32:13 +0530

    Create API integrations for Signup, verification and login

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,29 @@
+import axios from 'axios'
+import process from 'process'
+
+const server = axios.create({
+  baseURL: process.env.TEXTLOOKER_BACKEND_URL,
+  timeout: 1000,
+  headers: {'Content-Type': 'application/json'}
+})
+
 let api = {
-  login: (username:string, password:string):boolean => true
+  signup: async (email:string, password:string):Promise<boolean> => 
+    server.post('/user_registrations', { email, password })
+      .then(response => response.status === 200)
+      .catch(response => response.status === 401)
+  ,
+
+  verify: async (email:string, password:string, verificationToken:string):Promise<boolean> => 
+    server.post('/users', { email, password, verification_token: verificationToken })
+      .then(response => response.status === 200)
+      .catch(response => response.status === 401)
+  ,
+ 
+  login: async (email:string, password:string):Promise<any|[boolean, string]> => 
+    server.post('/login', { email, password })
+      .then(response => [response.status === 200, response.data.token])
+      .catch(error => [false, 'token not available'])
 }
 
 export default api
--- spec/api.test.ts
+++ spec/api.test.ts
@@ -1,10 +1,30 @@
 import api from '../api'
 
-describe('#login', () => {
-  test('it contains login function', () => {
-    const username = 'user'
-    const password = 'password'
+describe('Signup and login', () => {
 
-    expect(api.login(username, password)).toBe(true)
+  const email = 'testuser@gmail.com'
+  const password = 'testpassword'
+
+  xtest('returns true on successful sign up', async () => {
+    expect(api.signup(email, password)).toEqual(true)
+  })
+ 
+  xtest('returns true on successful verification', async () => {
+    expect(api.verify(email, password, verificationToken)).toEqual(true)
+  })
+
+  test('returns status(true) and token on successful login', async () => {
+
+    expect(await api.login(email, password)).toEqual(
+      [true, expect.any(String)]
+    )
+  })
+
+  test('returns status(false) on failed login', async () => {
+    const password = 'wrongpassword'
+
+    expect(await api.login(email, password)).toEqual(
+      [false, expect.any(String)]
+    )
   })
 })
