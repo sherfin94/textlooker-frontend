502035cd8536e4578f3481fa5bedc6567f0b0eac
Sherfin Shamsudeen <sherfin94@gmail.com>
2021-05-19 21:19:21 +0530

    Integrate with source listing API

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import process from 'process'
+import Source from './source'
 
 const server = axios.create({
   baseURL: process.env.TEXTLOOKER_BACKEND_URL,
@@ -20,13 +21,13 @@ let api = {
       .catch(_ => false)
   ,
  
-  login: async (email:string, password:string):Promise<[boolean, string]|any> => 
+  login: async (email:string, password:string):Promise<[boolean, string]|any[]> => 
     server.post('/login', { email, password })
       .then(response => [response.status === 200, response.data.token])
       .catch(_ => [false, 'token not available'])
   ,
 
-  refreshToken: async (token:string):Promise<[boolean,string]|any> =>
+  refreshToken: async ():Promise<[boolean,string]|any[]> =>
     server.get('/auth/refresh_token')
       .then(response => [response.status === 200, response.data.token])
       .catch(_ => [false, 'refresh not available'])
@@ -36,6 +37,17 @@ let api = {
     server.post('/auth/sources', { name })
       .then(response => response.status === 200)
       .catch(_ => false)
+  ,
+
+  getSources: async ():Promise<[boolean,Source[]]|any[]> =>
+    server.get('auth/sources')
+      .then(response => [
+        response.status === 200,
+        response.data.sources.map(
+          (source: {id:number, name:string}) => new Source(source.id, source.name))
+      ])
+      .catch(_ => [false, 'refresh not available'])
+  ,
 }
 
 
