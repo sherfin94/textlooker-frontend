b68c20f08c0c8e66f3392e36ed20c1d23c5ad25b
Sherfin Shamsudeen <sherfin94@gmail.com>
2021-05-19 21:03:23 +0530

    Integrate with source creation API

--- api/index.ts
+++ api/index.ts
@@ -11,19 +11,32 @@ let api = {
   signup: async (email:string, password:string):Promise<boolean> => 
     server.post('/user_registrations', { email, password })
       .then(response => response.status === 200)
-      .catch(response => response.status === 401)
+      .catch(_ => false)
   ,
 
   verify: async (email:string, verificationToken:string):Promise<boolean> => 
     server.post('/users', { email, verificationToken: verificationToken })
       .then(response => response.status === 200)
-      .catch(response => response.status === 401)
+      .catch(_ => false)
   ,
  
-  login: async (email:string, password:string):Promise<any|[boolean, string]> => 
+  login: async (email:string, password:string):Promise<[boolean, string]|any> => 
     server.post('/login', { email, password })
       .then(response => [response.status === 200, response.data.token])
-      .catch(error => [false, 'token not available'])
+      .catch(_ => [false, 'token not available'])
+  ,
+
+  refreshToken: async (token:string):Promise<[boolean,string]|any> =>
+    server.get('/auth/refresh_token')
+      .then(response => [response.status === 200, response.data.token])
+      .catch(_ => [false, 'refresh not available'])
+  ,
+
+  createSource: async (name:string):Promise<boolean> => 
+    server.post('/auth/sources', { name })
+      .then(response => response.status === 200)
+      .catch(_ => false)
 }
+
 
 export default api
