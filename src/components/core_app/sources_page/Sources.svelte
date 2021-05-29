2ef7e1d38e116df7ed9562715032b05199fc9230
Sherfin Shamsudeen <sherfin94@gmail.com>
2021-05-29 17:20:39 +0530

    Move sources table to a seperate component

---
+++ src/components/core_app/sources_page/Sources.svelte
@@ -0,0 +1,45 @@
+<script lang='typescript'>
+  import type { source } from '../../../interface'
+
+  export let sources:source[]
+
+  let sourceSlices = (sources:source[]):source[][] => {
+    let slices:source[][] = []
+    for(let i = 0; i < sources.length; i += 4) {
+      const slice = sources.slice(i, i + 4)
+      while(slice.length !== 0 && slice.length < 4) slice.push({id: null, name: null})
+      if (slice.length !== 0) slices.push(slice)
+    }
+    return slices
+  }
+
+</script>
+
+<div class="container">
+  {#each sourceSlices(sources) as slice}
+  <div class="columns">
+    {#each slice as source}
+    <div class="column p-0">
+        {#if source.id !== null}
+          <div class="block p-3">
+            <span class="icon has-text-success">
+              <i class="fas fa-database"></i>
+            </span>
+            {source.name}
+          </div>
+        {/if}
+      </div>
+    {/each}
+  </div>
+  {/each}
+</div>
+
+<style lang='scss'>
+  div.block {
+    border-radius: 4px;
+  }
+  div.block:hover {
+    background-color: aliceblue;
+    cursor: pointer;
+  }
+</style>
--- src/components/core_app/sources_page/SourcesPage.svelte
+++ src/components/core_app/sources_page/SourcesPage.svelte
@@ -1,28 +1,22 @@
 <script lang='typescript'>
   import { onMount } from 'svelte'
-  import type { source } from '../../../interface'
   import { fetchSources } from '../../../models/source'
+  import { sources as sourceStore } from '../../../store'
   import SearchBox from './SearchBox.svelte'
-  import { sources } from '../../../models/source'
+  import Sources from './Sources.svelte'
+  import type { source } from '../../../interface'
 
   let searchText = ''
   let loading = false
-  let sourceSlices:source[][] = []
+  let sources:source[]
 
-  let createSourceSlices = ():source[][] => {
-    let slices:source[][] = []
-    for(let i = 0; i < sources.length; i += 4) {
-      const slice = sources.slice(i, i + 4)
-      while(slice.length !== 0 && slice.length < 4) slice.push({id: null, name: null})
-      if (slice.length !== 0) slices.push(slice)
-    }
-    return slices
-  }
+  sourceStore.subscribe(sourceData => {
+    sources = sourceData
+  })
 
   onMount(async () => {
     loading = true
     await fetchSources()
-    sourceSlices = createSourceSlices()
     loading = false
   })
 
@@ -35,32 +29,6 @@
     <progress class="progress is-large is-info {loading ? '':'is-invisible'}" max="100" />
   </div>
 
-  <div class="container">
-    {#each sourceSlices as slice}
-    <div class="columns">
-      {#each slice as source}
-      <div class="column p-0">
-          {#if source.id !== null}
-            <div class="block p-3">
-              <span class="icon has-text-success">
-                <i class="fas fa-database"></i>
-              </span>
-              {source.name}
-            </div>
-          {/if}
-        </div>
-      {/each}
-    </div>
-    {/each}
-  </div>
+  <Sources bind:sources={sources} />
 </section>
 
-<style lang='scss'>
-  div.block {
-    border-radius: 4px;
-  }
-  div.block:hover {
-    background-color: aliceblue;
-    cursor: pointer;
-  }
-</style>
