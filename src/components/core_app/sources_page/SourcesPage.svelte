71dd2c06389ca69531ed92546126cf1d9765f6e1
Sherfin Shamsudeen <sherfin94@gmail.com>
2021-05-29 16:12:51 +0530

    Display sources as blocks

--- src/components/core_app/sources_page/SourcesPage.svelte
+++ src/components/core_app/sources_page/SourcesPage.svelte
@@ -13,6 +13,7 @@
     let slices:source[][] = []
     for(let i = 0; i < sources.length; i += 4) {
       const slice = sources.slice(i, i + 4)
+      while(slice.length !== 0 && slice.length < 4) slice.push({id: null, name: null})
       if (slice.length !== 0) slices.push(slice)
     }
     return slices
@@ -38,15 +39,28 @@
     {#each sourceSlices as slice}
     <div class="columns">
       {#each slice as source}
-        <div class="column">
-          <div class="block">
-            {source.name}
-          </div>
+      <div class="column p-0">
+          {#if source.id !== null}
+            <div class="block p-3">
+              <span class="icon has-text-success">
+                <i class="fas fa-database"></i>
+              </span>
+              {source.name}
+            </div>
+          {/if}
         </div>
       {/each}
     </div>
     {/each}
   </div>
-
-  
 </section>
+
+<style lang='scss'>
+  div.block {
+    border-radius: 4px;
+  }
+  div.block:hover {
+    background-color: aliceblue;
+    cursor: pointer;
+  }
+</style>
